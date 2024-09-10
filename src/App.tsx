66ca79e4e040341098ff1ef1249d6b5801628627
Michel John <michel.santos@dataside.com.br>
2024-09-10 19:33:28 -0300

    chore: add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import './App.css';
-
 function App() {
-  return (
-    <div>
-      <p>Hello!</p>
-    </div>
-  );
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
