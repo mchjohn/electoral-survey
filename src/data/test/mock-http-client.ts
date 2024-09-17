97bee03071307b2c945f326653e9504ef2d3654c
Michel John <michel.santos@dataside.com.br>
2024-09-16 21:46:31 -0300

    refactor: move HttpPostClientSpy to mock folder

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,10 @@
+import { IHttpPostClient } from '../protocols/https/http-post-client';
+
+export class HttpPostClientSpy implements IHttpPostClient {
+  url?: string;
+
+  async post(url: string): Promise<void> {
+    this.url = url;
+    return Promise.resolve();
+  }
+}
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,19 +1,10 @@
 import { describe, expect, test } from 'vitest';
 
-import { IHttpPostClient } from '@/data/protocols/https/http-post-client';
+import { HttpPostClientSpy } from '@/data/test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements IHttpPostClient {
-      url?: string;
-
-      async post(url: string): Promise<void> {
-        this.url = url;
-        return Promise.resolve();
-      }
-    }
-
     const httpPostClientSpy = new HttpPostClientSpy();
     const url = 'any_url';
 
