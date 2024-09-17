d2a3fa934e138dd48eb060a617685f3a6f6b2b36
Michel John <michel.santos@dataside.com.br>
2024-09-16 21:51:16 -0300

    refactor: move SUT creation toa a factory method

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -3,12 +3,24 @@ import { describe, expect, test } from 'vitest';
 import { HttpPostClientSpy } from '@/data/test/mock-http-client';
 import { RemoteAuthentication } from './remote-authentication';
 
+type SutTypes = {
+  sut: RemoteAuthentication;
+  httpPostClientSpy: HttpPostClientSpy;
+};
+
+// Design pattern Factory
+const makeSut = (url = 'any_url'): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy();
+  const sut = new RemoteAuthentication(url, httpPostClientSpy); // System under test: Objeto sendo testado [RemoteAuthentication]
+
+  return { sut, httpPostClientSpy };
+};
+
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    const httpPostClientSpy = new HttpPostClientSpy();
     const url = 'any_url';
+    const { sut, httpPostClientSpy } = makeSut(url);
 
-    const sut = new RemoteAuthentication(url, httpPostClientSpy); // System under test: Objeto sendo testado [RemoteAuthentication]
     await sut.auth();
 
     expect(httpPostClientSpy.url).toBe(url);
