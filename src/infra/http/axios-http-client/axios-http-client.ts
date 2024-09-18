646cf05b0d22250391f5454163d3d2a606c7fbd2
Michel John <michel.santos@dataside.com.br>
2024-09-18 19:15:53 -0300

    feat: ensure AxiosHttpClient returns correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,23 @@
-import { HttpPostParams } from '@/data/protocols/http';
 import axios from 'axios';
 
-export class AxiosHttpClient {
-  async post(params: HttpPostParams<any>): Promise<void> {
-    await axios.post(params.url, params.body);
+import {
+  HttpPostParams,
+  HttpResponse,
+  IHttpPostClient,
+} from '@/data/protocols/http';
+
+/**
+ * Design pattern Adapter
+ * Adaptando HttpPostClient e axios
+ */
+export class AxiosHttpClient implements IHttpPostClient<any, any> {
+  // eslint-disable-next-line class-methods-use-this
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body);
+
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    };
   }
 }
