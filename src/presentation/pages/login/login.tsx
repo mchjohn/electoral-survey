108965d5c590f0ac1c73a69ac5f1ed72917813f3
Michel John <michel.santos@dataside.com.br>
2024-09-23 10:31:09 -0300

    feat: add context to control display error and spinner on login screen

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,33 +1,54 @@
+import { useState } from 'react';
 import { AiOutlineLoading } from 'react-icons/ai';
 
 import { AuthHeader, AuthLink, Footer } from '@/presentation/components';
 import { Button } from '@/presentation/components/ui/button';
 import { Input } from '@/presentation/components/ui/input';
+import { FormContext } from '@/presentation/contexts/form/form-context';
+
+type FormStateProps = {
+  isLoading: boolean;
+  errorMessage: string;
+};
 
 export function Login() {
+  const [formState, setFormState] = useState<FormStateProps>({
+    isLoading: false,
+    errorMessage: '',
+  });
+
   return (
     <div className="flex h-screen w-full flex-col">
       <AuthHeader />
 
-      <form action="" className="text-zinc-50 m-auto w-10/12 md:w-96">
-        <h2 className="text-lg mb-4">Login</h2>
+      <FormContext.Provider value={formState}>
+        <form action="" className="text-zinc-50 m-auto w-10/12 md:w-96">
+          <h2 className="text-lg mb-4">Login</h2>
 
-        <div className="flex flex-col gap-4 md:p-10 md:bg-zinc-900 rounded-md">
-          <Input type="email" placeholder="Digite seu e-mail" />
-          <Input type="password" placeholder="Digite sua senha" />
+          <div
+            className="flex flex-col gap-4 md:p-10 md:bg-zinc-900 rounded-md"
+            data-testid="form-content"
+          >
+            <Input type="email" placeholder="Digite seu e-mail" />
+            <Input type="password" placeholder="Digite sua senha" />
 
-          <span className="text-sm text-red-400 -mt-2">
-            E-mail e ou senha inválidos
-          </span>
+            {formState.errorMessage && (
+              <span className="text-sm text-red-400 -mt-2">
+                E-mail e ou senha inválidos
+              </span>
+            )}
 
-          <Button type="submit">
-            <AiOutlineLoading className="mr-2 h-4 w-4 animate-spin" />
-            Entrar
-          </Button>
-        </div>
+            <Button type="submit" data-testid="form-button">
+              {formState.isLoading && (
+                <AiOutlineLoading className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              Entrar
+            </Button>
+          </div>
 
-        <AuthLink text="Criar uma conta" />
-      </form>
+          <AuthLink text="Criar uma conta" />
+        </form>
+      </FormContext.Provider>
 
       <Footer />
     </div>
