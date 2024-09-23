c0a22fee2e3c3e235f68a83ca9ed2262346e48b0
Michel John <michel.santos@dataside.com.br>
2024-09-23 20:01:05 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,25 +1,44 @@
-import { render } from '@testing-library/react';
+import { render, RenderResult } from '@testing-library/react';
 import { describe, expect, test } from 'vitest';
 
 import { Login } from './login';
 
+type SutTypes = {
+  sut: RenderResult;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />);
+
+  return { sut };
+};
+
 describe('Login component', () => {
   test('Should not render error on start app', () => {
-    const { getByTestId } = render(<Login />);
+    const {
+      sut: { getByTestId },
+    } = makeSut();
+
     const formContent = getByTestId('form-content');
 
     expect(formContent.getElementsByTagName('span').length).toBe(0);
   });
 
   test('Should not render spinner inside the button on start app', () => {
-    const { getByTestId } = render(<Login />);
+    const {
+      sut: { getByTestId },
+    } = makeSut();
+
     const formButton = getByTestId('form-button');
 
     expect(formButton.childElementCount).toBe(0);
   });
 
   test('Should start with the button disabled when starting the app', () => {
-    const { getByTestId } = render(<Login />);
+    const {
+      sut: { getByTestId },
+    } = makeSut();
+
     const formButton = getByTestId('form-button') as HTMLButtonElement;
 
     expect(formButton.disabled).toBe(true);
