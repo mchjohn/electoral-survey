920ebce2011647c8f536cac0ca892ad4e24fbce2
Michel John <michel.santos@dataside.com.br>
2024-09-23 10:04:15 -0300

    chore: adding testing library

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
+/// <reference types="vitest" />
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
