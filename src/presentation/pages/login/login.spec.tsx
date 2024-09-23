b108f698e6bae9ea82ffd828e53aa8e904b2d265
Michel John <michel.santos@dataside.com.br>
2024-09-23 10:31:24 -0300

    test: add context to control display error and spinner on login screen

---
+++ src/presentation/pages/login/login.spec.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+
+import { Login } from './login';
+
+describe('Login component', () => {
+  test('Should not render error on start app', () => {
+    const { getByTestId } = render(<Login />);
+    const formContent = getByTestId('form-content');
+
+    expect(formContent.getElementsByTagName('span').length).toBe(0);
+  });
+
+  test('Should not spinner inside the button on start app', () => {
+    const { getByTestId } = render(<Login />);
+    const formContent = getByTestId('form-button');
+
+    expect(formContent.childElementCount).toBe(0);
+  });
+});
